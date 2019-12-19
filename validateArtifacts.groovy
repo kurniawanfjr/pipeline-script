9d139257d3d4417438a3974ab2a207708f5b80e2
Fajar Kurniawan <fajar.kurniawan@bukalapak.com>
2019-12-20 01:59:49 +0700

    asada

--- validateArtifacts.groovy
+++ validateArtifacts.groovy
@@ -13,10 +13,10 @@ def validateCreatedFileWithin2Weeks() {
     sh '''
         if [[ $(gdrive list -m 100 -q "createdTime < '${date2WeeksAgo}'" | awk '/MB/{print}') ]]
         then
-            echo 'There are several files above 2 weeks ago'
+            echo "There are several files above 2 weeks ago"
             gdrive delete $(gdrive list -m 100 -q "createdTime < ''${date2WeeksAgo}'" | awk '/MB/{print $1}')
         else
-            echo 'There's nothing files above 2 weeks ago'
+            echo "There's nothing files above 2 weeks ago"
     '''
 }
 
